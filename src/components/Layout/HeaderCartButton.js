177017370948faaca372a56c357db095c3362007
Prannat Jain <prannatj@gmail.com>
2023-01-05 01:22:06 +0530

    useEffect hook for animation

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,19 +1,42 @@
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
 
   //amount = quantity, reduce function finds quantity of each type of food
-  const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+  const numberOfCartItems = items.reduce((currentNumber, item) => {
     return currentNumber + item.amount;
   }, 0);
 
+  //if btnishighlighted is true, then btnclasses includes the animation otherwise just an empty string is added to classes
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    //timer for animation to start
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+    //cleanup function of useEffect
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
     //onclick coming from props is passed to inbuilt onclick of button tag in html
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon></CartIcon>
       </span>
